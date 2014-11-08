0f074f841a9e16481266761069c7eaecb72a609c
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-08 15:19:13 +0100

    Capitalized parameter names to match the offical reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-//! The library facilitates static linking with the [Basic Linear Algebra
-//! Subprograms][1].
+//! Facilitation of static linking with the [Basic Linear Algebra Subprograms][1].
 //!
 //! [1]: http://www.netlib.org/blas/
 
@@ -12,16 +11,16 @@ pub use dgemv_ as dgemv;
 
 #[link(name = "gfortran")]
 extern {
-    /// http://www.netlib.org/lapack/explore-html/dc/da8/dgemv_8f.html
-    fn dgemv_(trans: *const c_char, m: *const c_int, n: *const c_int,
-              alpha: *const c_double, a: *const c_double, lda: *const c_int,
-              x: *const c_double, incx: *const c_int, beta: *const c_double,
-              y: *mut c_double, incy: *const c_int);
-
     /// http://www.netlib.org/lapack/explore-html/d7/d2b/dgemm_8f.html
-    fn dgemm_(transa: *const c_char, transb: *const c_char, m: *const c_int,
-              n: *const c_int, k: *const c_int, alpha: *const c_double,
-              a: *const c_double, lda: *const c_int, b: *const c_double,
-              ldb: *const c_int, beta: *const c_double, c: *mut c_double,
-              ldc: *const c_int);
+    fn dgemm_(TRANSA: *const c_char, TRANSB: *const c_char, M: *const c_int,
+              N: *const c_int, K: *const c_int, ALPHA: *const c_double,
+              A: *const c_double, LDA: *const c_int, B: *const c_double,
+              LDB: *const c_int, BETA: *const c_double, C: *mut c_double,
+              LDC: *const c_int);
+
+    /// http://www.netlib.org/lapack/explore-html/dc/da8/dgemv_8f.html
+    fn dgemv_(TRANS: *const c_char, M: *const c_int, N: *const c_int,
+              ALPHA: *const c_double, A: *const c_double, LDA: *const c_int,
+              X: *const c_double, INCX: *const c_int, BETA: *const c_double,
+              Y: *mut c_double, INCY: *const c_int);
 }
