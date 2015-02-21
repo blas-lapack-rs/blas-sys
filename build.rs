0c5e03f77f01445e042556909d282dfbc5dff319
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-21 15:24:11 +0100

    Switched to the new path

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
-#![feature(env, old_io, old_path)]
+#![feature(env, path, process)]
 
-use std::old_io as io;
-use std::env;
+use std::{env, process};
+use std::path::PathBuf;
 
 macro_rules! cmd(
-    ($name:expr) => (io::process::Command::new($name));
+    ($name:expr) => (process::Command::new($name));
 );
 
 macro_rules! fmt(
@@ -17,23 +17,27 @@ macro_rules! get(
 
 macro_rules! run(
     ($command:expr) => (
-        assert!($command.stdout(io::process::InheritFd(1))
-                        .stderr(io::process::InheritFd(2))
+        assert!($command.stdout(process::Stdio::inherit())
+                        .stderr(process::Stdio::inherit())
                         .status().unwrap().success());
     );
 );
 
 fn main() {
-    let from = Path::new(get!("CARGO_MANIFEST_DIR")).join("blas");
-    let into = Path::new(get!("OUT_DIR"));
+    let mut from = PathBuf::new(&get!("CARGO_MANIFEST_DIR"));
+    from.push("blas");
 
-    run!(cmd!("cmake").cwd(&into).arg(&from)
-                                 .arg("-DCMAKE_Fortran_FLAGS='-O2 -frecursive -fPIC'"));
+    let into = PathBuf::new(&get!("OUT_DIR"));
 
-    run!(cmd!("cmake").cwd(&into).arg("--build").arg(".")
-                                 .arg("--target").arg("blas")
-                                 .arg("--")
-                                 .arg(fmt!("-j{}", get!("NUM_JOBS"))));
+    run!(cmd!("cmake").current_dir(&into)
+                      .arg(&from)
+                      .arg("-DCMAKE_Fortran_FLAGS='-O2 -frecursive -fPIC'"));
+
+    run!(cmd!("cmake").current_dir(&into)
+                      .arg("--build").arg(".")
+                      .arg("--target").arg("blas")
+                      .arg("--")
+                      .arg(fmt!("-j{}", get!("NUM_JOBS"))));
 
     println!("cargo:rustc-flags=-L {}", into.join("lib").display());
     println!("cargo:rustc-flags=-l blas:static");
