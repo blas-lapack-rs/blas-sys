463c44cb479e3b721034c2f21d0791043880321c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-14 10:11:25 -0400

    Get rid of CBLAS

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,28 +1,7 @@
 extern crate libblas_sys as raw;
 
 #[test]
-fn link_to_c() {
-    let order = raw::CblasRowMajor;
-    let trans = raw::CblasNoTrans;
-    let m = 1;
-    let n = 1;
-    let alpha = 0.0;
-    let a = vec![0.0];
-    let lda = 1;
-    let x = vec![0.0];
-    let incx = 1;
-    let beta = 0.0;
-    let mut y = vec![0.0];
-    let incy = 1;
-
-    unsafe {
-        raw::cblas_dgemv(order, trans, m, n, alpha, a.as_ptr(), lda,
-                         x.as_ptr(), incx, beta, y.as_mut_ptr(), incy);
-    }
-}
-
-#[test]
-fn link_to_fortran() {
+fn link() {
     let trans = b'N';
     let m = 1;
     let n = 1;
