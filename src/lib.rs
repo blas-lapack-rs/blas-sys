bfa4a3666c82cbc6752c1eb58a862c80a2141b6e
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-16 21:20:51 +0100

    Remove periods from type descriptions

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@
 
 extern crate libc;
 
-/// A complex number with 64-bit parts.
+/// A complex number with 64-bit parts
 #[allow(bad_style)]
 pub type c_double_complex = [libc::c_double; 2];
 
-/// A complex number with 32-bit parts.
+/// A complex number with 32-bit parts
 #[allow(bad_style)]
 pub type c_float_complex = [libc::c_float; 2];
 
